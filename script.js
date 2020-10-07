e6ad9f26999f565b745790a42136cb5274488428
Irvin Cossio chavalier <70815810+irvinChavMB@users.noreply.github.com>
2020-10-06 20:40:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,200 @@
+const happyCards = [
+  {
+    name: 'Richi',
+    detail: `Feliz Cumple Dany!!  Todo lo mejor hoy en tu día!! 
+    te mando un abrazo!`,
+    picture: 'https://2.bp.blogspot.com/-GYlAh8W09DY/VqTFGfuT3DI/AAAAAAAABBU/zbqHzIcEses/s1600/el%2Bcazador.png'
+  },
+  {
+    name: `Carlita`,
+    detail: `Mil mil felicidades Dani <3
+    Gracias por aguantarme los perreos y las cumbias en las fiestas hahaha Todo lo mejor!!!
+    Atte. Carlita`,
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRhwpkOknvY95dnnpzAzqyF0F2K818dz4C7Yw&usqp=CAU'
+  },
+  {
+    name: 'Tincho!!',
+    detail: `Feliz cumple danielsan,
+    espero que la pases super bien, te deseo todo lo mejor , un abrazo.`,
+    picture: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT999ApdirqZ67xYum_jXeKXn-PI2yKqKk7-A&usqp=CAU'
+  },
+  {
+    name: 'Fa',
+    detail: `Feliz cumple Dan!!
+    Te tengas un bonito día junto a tus seres queridos.
+    Mis mejores deseos para tus proyectos actuales uñy futuros! `,
+    picture: 'https://3.bp.blogspot.com/-GtdmvmToh84/VqTFIDtt0cI/AAAAAAAABDs/OCIhojBNlRU/s1600/el%2Bprotector.png'
+  },
+  {
+    name: 'Mariana D.',
+    detail: `Dani, ¡feliz cumpleaños! espero que estés teniendo un excelente día.
+    Te deseo muchas cosas buenas hoy y siempre.
+    Te mando un abrazo fuerte.`,
+    picture: 'https://4.bp.blogspot.com/-ApaP-jsIsQc/VqTFIltkFgI/AAAAAAAABC4/pwAbmzxrGI4/s1600/el%2Btonto%2Bde%2Bla%2Baldea.png'
+  },
+  {
+    name: 'Vani ^-^',
+    detail: `Feliz cumple Dani!!!
+    Sigue adelante con cada meta propuesta! Eres una persona super increíble!
+    Hay que celebrar con otra noche de anime/series, hasta que podamos volver a conciertos :D
+    Un fuerte abrazo!
+    
+    Atte: Vani ^-^`,
+    picture: 'https://4.bp.blogspot.com/-d0V6uFjiBkE/VqTFHqKVz_I/AAAAAAAABB0/DvhY9VxX9CI/s1600/el%2Bperro%2Blobo.png'
+  },
+  {
+    name: 'Gus',
+    detail: `Dani! Dross! Yisus! Dani-san! Lobo mayor! tantos estilos che xD, muchas felicidades,
+    espero que estés jugando mucho y pidiendo juanquis por delivery.
+    Un abrazo de lobo xD
+    Atte: Gus`,
+    picture: 'https://2.bp.blogspot.com/-rA7XuxtkYf4/VqTFFbUI00I/AAAAAAAABD8/cTZltyywEwg/s1600/el%2Baldeano%2Bcom%25C3%25BAn.png'
+  },
+  {
+    name: 'La Danny',
+    detail: `Dan Feliz Cumple!!! Todo lo mejor en tu día, se te quiere mucho,
+    aunque te haga renegar aveces :p
+    ya vamos a celebrar con su debido Juanqui cuando se pueda. 
+    - Danny`,
+    picture: 'https://2.bp.blogspot.com/-OFt2Wkr1O_E/VqTFG_1DIYI/AAAAAAAABDk/5wAs2f5Tuig/s1600/el%2Bcuervo.png'
+  },
+  {
+    name: '🤗~ Pam',
+    detail: `Daniiii! muchas felicidades en tu cumple!
+    🥳recuerdo que la primera vez que te conocí te tuve miedo jaja los días que jugamos lobo fueron memorables,
+    creo que gracias a eso, nos conocimos más en la comunidad.
+    Eres un gran amigo Dani, te aprecio mucho, gracias por siempre bendecirnos también jajaja te mando un fuerte abrazo`,
+    picture: 'https://1.bp.blogspot.com/-Ep0EjRHHCbE/VqTFG4ydp9I/AAAAAAAABBM/-0QWBonoR_I/s1600/el%2Bdomador%2Bde%2Bosos.png'
+  },
+  {
+    name: 'Brian Vega',
+    detail: `Dani!! crack!! master!! titan!! leyenda!! Feliz cumpleaños!
+    espero que estes comprando nuevos juegos y pasándola bien, mantén siempre esa buena onda,
+    haber cuando tenemos una change de jugar otra vez, Mil felicidades!!`,
+    picture: 'https://mesadejuegos.top/wp-content/uploads/2019/12/el-caballero-de-la-espada-oxidada.jpg'
+  },
+  {
+    name: 'Susy ♡',
+    detail: `Feliz Cumple querido Dani, que la pases super bien,
+    te deseo todo lo mejor hoy y siempre. Eres una grandiosa persona y sobre todo un gran amigo.
+    Se te quiere mucho`,
+    picture: 'https://2.bp.blogspot.com/-GYlAh8W09DY/VqTFGfuT3DI/AAAAAAAABBU/zbqHzIcEses/s1600/el%2Bcazador.png'
+  },
+  {
+    name: 'Lu',
+    detail: `Mi Daaan! Feliz cumplee! Que todos tus deseos se cumplan, hoy y en cada momento de tu vida, eres un amigo valioso con qyuien los viajes, momentos y todoo son increibles experiencias.
+    Gracias por llegar a mi vida y ser alguien tan maravilloso.
+    Te quiere muchisimoo.`,
+    picture: ''
+  },
+  {
+    name: '~Fer',
+    detail: `Feliz cumple Dani!! espero que realmente la hayas pasado super bien y que hayas comido muchos juanquis,
+    eres una persona que inspira un montón, tiene buena actitud para todo y ademas alguien bastante gracioso.
+    Espero que se te cumplan todos tus sueños y metas. `,
+    picture: 'https://4.bp.blogspot.com/-PvZAko_DzFM/VqTFIsX6GXI/AAAAAAAABDQ/8oDe4i-MgCs/s1600/el%2Bzorro.png'
+  },
+  {
+    name: 'Dans',
+    detail: `Feliz cumple Dani! Espero que la pases genial este día. Nunca cambies,
+    sigue siendo esa persona genial.`,
+    picture: 'https://4.bp.blogspot.com/-JYcOxgPUIZ4/VqTFHgmiMgI/AAAAAAAABD0/Ok0X2AYPu4A/s1600/el%2Bni%25C3%25B1o%2Bsalvaje.png'
+  },
+  {
+    name: 'El Carlits xD',
+    detail: `Feliz Cumpleaños Dany!
+    Espero que hayas pasado un día genial, comiendo muy rico!
+    Eres una gran persona y espero que siempre mantengas toda la buena onda que nos contagias y por la que todos te queremos un montón!
+    Gracias por tu amistad!
+    Un abrazo fuerte!`,
+    picture: 'https://mesadejuegos.top/wp-content/uploads/2019/12/el-piromano.jpg'
+  },
+  {
+    name: 'Griss',
+    detail: `Feliz Cumple Dani, espero que la pases mega, archi bien.
+    Te deseo muchísimo éxito y ya estaremos jugando más jueguillos en más reuniones.`,
+    picture: 'https://mesadejuegos.top/wp-content/uploads/2019/12/las-dos-hermanas.jpg'
+  },
+  {
+    name: 'Mauri',
+    detail: `¡Dani! ¡Mil felicidades viejo! Espero estés pasando un día genial,
+    eres un tipazo, no mereces nada menos.
+    Nunca pares de narrar esas partidas de hombres lobo de Castronegro de esa forma tan épica xD`,
+    picture: 'https://4.bp.blogspot.com/-JqdMeo48ayA/VqTFGG9JACI/AAAAAAAABD8/0xRxY2nPZ1Y/s1600/el%2Bcabeza%2Bde%2Bturco.png'
+  },
+  {
+    name: 'Ivo',
+    detail: `Feliz cumple Dan espero la pases muy bien estimau!
+    segui con la buena onda de siempre, te mando un fuerte abrazo`,
+    picture: 'https://1.bp.blogspot.com/-adtBSBbEdcs/VqTFFZfjGNI/AAAAAAAABAg/sZUXSIOSxN8/s1600/cupido.png'
+  },
+  {
+    name: 'Julio Silva',
+    detail: `Dani!
+    Feliz cumpleaños 🎁🥳🎁
+    Espero que la pases muyyyy bien porque te mereces mucho por ser tan buen amigo como lo eres.
+    Cuídate bastante, te mando un fuerte abrazo!`,
+    picture: 'https://1.bp.blogspot.com/-OHQEJnG07Tw/VqTFJGIJLbI/AAAAAAAABDI/CVRZTSoO8H8/s1600/la%2Babnegada%2Bsirvienta.png'
+  },
+  {
+    name: 'Irvin',
+    detail: `Dani, espero que comieras juanquis,
+    abrazaras a tus consolas y fer te prepare un drama xD bueno feliz cumple
+    espero que te guste el detalle`,
+    picture: 'https://2.bp.blogspot.com/-K_aOvqqba5w/VqTFJaNp8-I/AAAAAAAABDY/HIZe6PslW4Y/s1600/la%2Bgitana.png'
+  },
+  {
+    name: 'Hormi',
+    detail: `Te quiero Dan
+    Gracias por todas las aventuras y por siempre estar ahí ♡ 
+    P.D. Ya me debes 2 conciertos.`,
+    picture: 'https://mesadejuegos.top/wp-content/uploads/2019/12/la-vidente.jpg'
+  },
+  {
+    name: '~Allan',
+    detail: `Feliz cumple Dani-san!
+    Espero que lo pases excelente y juegues mucho!
+    Que Arceus esté contigo y que nunca te falte un defuse en la mano.
+    Un abrazo!`,
+    picture: 'https://1.bp.blogspot.com/-fbtSN9gExlA/VqTFJE4kmjI/AAAAAAAABCY/WFx3blI6GUo/s1600/infecto%2Bpadre%2Bde%2Btodos%2Blos%2Blobos.png'
+  },
+  {
+    name: 'Antonio',
+    detail: `Dani!! muchas felicidades!!
+    ya sabes tienes el Power xD usalo con sabiduría. Te deseo lo mejor,
+    el mejor de los exitos y haber cuando jugamos exploding kittens. `,
+    picture: 'https://mesadejuegos.top/wp-content/uploads/2019/12/el-tonto-del-pueblo.jpg'
+  },
+  {
+    name: 'Miro',
+    detail: ``,
+    picture: 'https://4.bp.blogspot.com/-W6UhF4dZWQ0/VqTFJ7z2EoI/AAAAAAAABC0/9XqZwVjCpKk/s1600/lobo%2Bferoz.png'
+  },
+]
+
+function reveal(id) {
+  const item = happyCards[id];
+  if (item.name === 'Miro') {
+    window.location.href = window.location.href + "/CumpleDaniel.pdf";
+  }
+  $('#name').text(item.name);
+  $('#detail').text(item.detail);
+  $('#image').attr('src', item.picture);
+}
+
+let cardsHtml = happyCards.reduce((total, item, index) => total + `<button onclick="reveal(${index})">${item.name}</button>`, '')
+$('#cards').html(cardsHtml)
+
+const cards = $('#cards');
+const count = cards.children().length;
+const mid = (count/3);
+const maxRotation = 5;
+const cardHeight = 20;
+$('#cards button').each(function(i, it) {
+  const offset = i - mid + 0.5;
+  const multiplier = offset / (mid - 0.5);
+  $(this).css('transform', 'rotate('+ maxRotation * multiplier +'deg)');
+  $(this).css('top', 15 + Math.abs(offset) * 2 + 'px');
+});
+
+reveal(0)
